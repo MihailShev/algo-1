c7532b560f33dfa977b76be95b4cd48f9e5f78e7
Mikhail Shevchenko <mshev@list.ru>
2020-08-03 21:51:11 +0300

    Compiled exe

--- main.go
+++ main.go
@@ -10,14 +10,15 @@ const path1 = "test-data/0.String"
 const path2 = "test-data/1.Tickets"
 
 func main() {
-	fmt.Println("Test string length")
+	fmt.Printf("\n\n*** Test string length ***\n\n")
 
 	testLength := tester.NewTester(tasks.StringLength{}, path1)
 	testLength.RunTest()
 
-	fmt.Printf("=========================\n=========================\n")
-	fmt.Println("Test lucky tickets")
+	fmt.Printf("\n\n*** Test lucky tickets ***\n\n")
 
 	testLuckyTickets := tester.NewTester(tasks.LuckyTickets{}, path2)
 	testLuckyTickets.RunTest()
+
+	_, _ = fmt.Scanf(" ")
 }
